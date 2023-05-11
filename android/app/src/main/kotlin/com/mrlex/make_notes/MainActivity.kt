8fec8465e66c93f6d1abced1fb083a9458236315
amit-owninfotech <amit.owninfotech@gmail.com>
2023-05-11 12:26:56 +0530

    my first task

---
+++ android/app/src/main/kotlin/com/mrlex/make_notes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mrlex.make_notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text("My First Project Text I Have Done"),
+          ),
+        ),
+      ),
+    );
+  }
+}
